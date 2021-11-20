5e3e2eaf7c2ee37677fd88c1e1f9ed7271f37522
student <Student@Antos-iMac.local>
2021-11-19 17:20:12 -0800

    create the fake data

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -2,7 +2,30 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
-    res.send("API is working properly");
+    //res.send("API is working properly");
+    res.json({
+        data: [
+            {
+              name: 'Apple Watch Series 6',
+              brand: 'Apple',
+              price: 529
+            },
+            {
+              name: 'Apple Watch SE',
+              brand: 'Apple',
+              price: 369
+            },
+            {
+              name: 'Apple Watch Series 3',
+              brand: 'Apple',
+              price: 259
+            }
+          ]
+    });
 });
 
 module.exports = router;
+
+
+
+
